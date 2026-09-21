a20e5ae6b386e32cab913029b03e0d236971314e
salahdaddi <151565912+salahdaddi@users.noreply.github.com>
2026-09-21 19:03:05 +0100

    Update ddl_silver.sql

--- Scripts/Silver/ddl_silver.sql
+++ Scripts/Silver/ddl_silver.sql
@@ -21,10 +21,11 @@ IF OBJECT_ID('silver.crm_prd_info','U') IS NOT NULL
 
 CREATE TABLE silver.crm_prd_info(
 prd_id INT,
-prd_key VARCHAR(50),
-prd_nm VARCHAR(50),
+prd_cat NVARCHAR(50), 
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
 prd_cost INT,
-prd_line VARCHAR(50),
+prd_line NVARCHAR(50),
 prd_start_dt DATE,
 prd_end_dt DATE,
 dwh_create_date DATETIME2 DEFAULT GETDATE()
@@ -37,9 +38,9 @@ CREATE TABLE silver.crm_sales_details(
 sls_ord_num NVARCHAR(50),
 sls_prd_key NVARCHAR(50),
 sls_cust_id INT,
-sls_order_dt INT,
-sls_ship_dt INT,
-sls_due_dt INT,
+sls_order_dt DATE,
+sls_ship_dt DATE,
+sls_due_dt DATE,
 sls_sales INT,
 sls_quantity INT,
 sls_price INT,
@@ -77,3 +78,4 @@ subcat NVARCHAR(50),
 maintenance NVARCHAR(50),
 dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
+
